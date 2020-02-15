6f0a1c018f94a887e92151b27ebbd8935a34db21
DataJek <code@datajek.io>
2020-02-14 16:32:17 -0800

    Fri Feb 14 16:32:10 PST 2020

--- copyScript.sh
+++ copyScript.sh
@@ -1,9 +1,11 @@
 mkdir -p FirstCutJarDependencies
 mkdir -p SequenceFilesJarDependencies
+mkdir -p MapReduceJarDependencies
 
 rm -r FirstCutJarDependencies/*
 rm -r SequenceFilesJarDependencies/*
+rm -r MapReduceJarDependencies/*
 
 cp -R /Users/ch.afzal/Personal/Code/DataJek/BigDataCourse/SequenceFiles/resources/SequenceFilesJarDependencies/* SequenceFilesJarDependencies/
 cp -R /Users/ch.afzal/Personal/Code/DataJek/BigDataCourse/FirstCut/resources/FirstCutJarDependencies/* FirstCutJarDependencies/
-
+cp -R /Users/ch.afzal/Personal/Code/DataJek/BigDataCourse/MapReduce/resources/MapReduceJarDependencies/* MapReduceJarDependencies/
